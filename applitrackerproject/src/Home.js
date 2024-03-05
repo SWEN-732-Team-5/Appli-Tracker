4c680f18f05dc2c3fbdcba20645d3df850f24833
SoudagarLondheRIT <srl1622@g.rit.edu>
2024-03-05 00:18:50 -0500

    SignUp page added

---
+++ applitrackerproject/src/Home.js
@@ -0,0 +1,17 @@
+import React, { useState, useEffect } from 'react';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import './App.css';
+import Login from './Login';
+import SignUp from './SignUp';
+
+function Home() {
+
+  return (
+      <>
+        <h1>Homepage</h1>
+      </>
+  );
+}
+
+export default Home;
+
---
+++ applitrackerproject/src/SignUp.js
@@ -0,0 +1,92 @@
+import React, { useState } from 'react';
+import { Container, Row, Col, Form, FormGroup, Label, Input, Button, Alert } from 'reactstrap';
+import { Link } from "react-router-dom";
+
+import { useNavigate  } from 'react-router-dom';
+
+const SignUp = () => {
+ 
+  return (
+    <Container>
+      <Row className="justify-content-center">
+        <Col md={6}>
+          <h2 className="mt-4 mb-4">Signup</h2>
+          <Form >
+            <FormGroup row>
+              <Label for="email" sm={3}>
+                Email
+              </Label>
+              <Col sm={9}>
+                <Input
+                  type="email"
+                  name="email"
+                  id="email"
+                  placeholder="Enter your email"
+                  required
+                />
+              </Col>
+            </FormGroup>
+            <FormGroup row>
+              <Label for="position" sm={3}>
+                Position
+              </Label>
+              <Col sm={9}>
+                <Input
+                  type="text"
+                  name="position"
+                  id="position"
+                  placeholder="Enter your position"
+                  required
+                />
+              </Col>
+            </FormGroup>
+            <FormGroup row>
+              <Label for="password" sm={3}>
+                Password
+              </Label>
+              <Col sm={9}>
+                <Input
+                  type="password"
+                  name="password"
+                  id="password"
+                  placeholder="Enter your password"
+                  required
+                />
+              </Col>
+            </FormGroup>
+            <FormGroup row>
+              <Label for="confirmPassword" sm={3}>
+                Confirm Password
+              </Label>
+              <Col sm={9}>
+                <Input
+                  type="password"
+                  name="confirmPassword"
+                  id="confirmPassword"
+                  placeholder="Confirm your password"
+                  required
+                />
+              </Col>
+            </FormGroup>
+            {/* {passwordMatchError && (
+              <Alert color="danger" className="mt-3">
+                Passwords do not match. Please enter matching passwords.
+              </Alert>
+            )} */}
+
+            <Button color="primary" type="submit">
+              Sign Up
+            </Button>
+
+            <Link to="/login_nightclub">
+                  <Button color="primary">LogIn</Button>
+            </Link>
+
+          </Form>
+        </Col>
+      </Row>
+    </Container>
+  );
+};
+
+export default SignUp;
