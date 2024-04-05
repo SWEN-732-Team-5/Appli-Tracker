8c84cc1bb8af07760124a88e135ddd7af1e43302
SoudagarLondheRIT <srl1622@rit.edu>
2024-04-04 22:22:16 -0400

    Test Cases changed to Unit Testing

--- backend/controllers/jobCreationController.js
+++ backend/controllers/jobCreationController.js
@@ -9,3 +9,13 @@ module.exports = async function jobCreateController(requestBody)
     const savedJob = await newJob.save();
     return savedJob;
 }
+
+// module.exports = async function jobCreateController(requestBody)
+// {
+//     console.log("Request Body:", requestBody); // Log requestBody to verify it's correct
+//     const newJob = new JobCreationSchema(requestBody);
+//     console.log("New Job Instance:", newJob); // Log newJob instance to verify it's created correctly
+//     const savedJob = await newJob.save();
+//     console.log("Saved Job:", savedJob); // Log savedJob to verify it's created correctly
+//     return savedJob;
+// }
---
+++ backend/controllers/weeklyDashboardController.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+const JobCreationSchema = require('../models/jobCreationModel');
+
+
+module.exports = async function weeklyDashboardController(requestBody)
+{
+    try
+    {
+    const startDate = new Date(requestBody.startDate);
+    const endDate = new Date(requestBody.endDate);
+  
+    // Ensure endDate is at least a week after startDate (optional)
+  
+    const jobs = await JobCreationSchema.find({
+      applied_date: { $gte: startDate, $lt: endDate }
+    });
+    return jobs;
+  
+    } 
+    catch (error) 
+    {
+        console.error("Error occurred during stage update:", error);
+        throw new Error("An error occurred during stage updation.");
+    }
+
+}
