99fde6d9053aaaf8c806af7b7bcb8715f2ccd5e6
sj9017 <143730166+sj9017@users.noreply.github.com>
2024-04-19 19:17:19 -0400

    jobrole pie chart added

---
+++ applitrackerproject/src/components/JobRolePieChart.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import Plot from 'react-plotly.js';
+
+const JobRolePieChart = ({ data }) => {
+    const labels = data.map(item => item.role);
+    const values = data.map(item => item.count);
+
+    return (
+        <Plot
+            data={[
+                {
+                    labels: labels,
+                    values: values,
+                    type: 'pie',
+                    textinfo: 'label+percent',
+                    insidetextorientation: 'radial'
+                }
+            ]}
+            layout={{
+                height: 400,
+                width: 500,
+                title: 'Job Application by Role'
+            }}
+        />
+    );
+};
+
+export default JobRolePieChart;
