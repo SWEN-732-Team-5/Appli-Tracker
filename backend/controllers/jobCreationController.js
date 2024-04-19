06392d90809f97d7f647f74b9db44a32fce322b7
Parva Shah <parvashah310501@gmail.com>
2024-04-19 19:44:05 -0400

    Added sign up intergration

--- backend/controllers/jobCreationController.js
+++ backend/controllers/jobCreationController.js
@@ -5,6 +5,7 @@ const JobCreationSchema = require('../models/jobCreationModel');
 module.exports = async function jobCreateController(requestBody)
 {
     const newJob = new JobCreationSchema(requestBody);
+    
 
     const savedJob = await newJob.save();
     return savedJob;
--- backend/controllers/userSignUpController.js
+++ backend/controllers/userSignUpController.js
@@ -1,8 +1,9 @@
 const mongoose = require('mongoose');
 const userCreationSchema = require('../models/user');
 
-module.exports = async function userCreationSchema(requestBody)
+module.exports = async function userCreateSchema(requestBody)
 {
+    console.log(requestBody);
     const newUser = new userCreationSchema(requestBody);
 
     const savedUser = await newUser.save();
