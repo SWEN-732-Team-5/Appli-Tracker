59c19189f8b567a9760591a85700261ebfcc5c24
shardulcg17 <scg6975@g.rit.edu>
2024-04-21 18:09:57 -0400

    Issue resolved

--- applitrackerproject/src/Home.js
+++ applitrackerproject/src/Home.js
@@ -3,33 +3,12 @@ import Dashboard from './components/Dashboard';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
-// function Home() {
-
-//   return (
-//         <h1>Homepage</h1>
-//   );
-// }
-
-// export default Home;
-
-
-
 function Home() {
-  // const jobs = [
-  //   // This is placeholder data; you would replace this with actual data fetched from an API.
-  //   { id: 1, title: 'UI/UX Designer', company: 'Netflix', salary: '$12K-$14K / Month', type: 'Fulltime', description:'At Netflix, our mission is to Entertain the World. We are constantly innovating on how entertainment is imagined, created and delivered to a global audience. We currently stream content in more than 30 languages in 190 countries, topping over 233 million paid subscribers. '},
-  //   { id: 2, title: 'UX Designer', company: 'Triple Whale', salary: '$8K-$11K / Month', type: 'Fulltime', description:'At Netflix, our mission is to Entertain the World. We are constantly innovating on how entertainment is imagined, created and delivered to a global audience. We currently stream content in more than 30 languages in 190 countries, topping over 233 million paid subscribers. '},
-  //   { id: 3, title: 'Product Designer', company: 'Motto', salary: '$12K-$14K / Month', type: 'Part-time', description:'At Netflix, our mission is to Entertain the World. We are constantly innovating on how entertainment is imagined, created and delivered to a global audience. We currently stream content in more than 30 languages in 190 countries, topping over 233 million paid subscribers. '},
-  //   { id: 4, title: 'UI/UX Designer', company: 'Brooksource', salary: '$12K-$14K / Month', type: 'Fulltime', description:'At Netflix, our mission is to Entertain the World. We are constantly innovating on how entertainment is imagined, created and delivered to a global audience. We currently stream content in more than 30 languages in 190 countries, topping over 233 million paid subscribers. '},
-  //   { id: 5, title: 'UI Designer', company: 'SideQuestVR', salary: '$12K-$14K / Month', type: 'Fulltime' , description:'At Netflix, our mission is to Entertain the World. We are constantly innovating on how entertainment is imagined, created and delivered to a global audience. We currently stream content in more than 30 languages in 190 countries, topping over 233 million paid subscribers. '},
-  //   // ... more job listings
-  // ];
 
   const [jobs, setJobDetail] = useState([]);
 
   const fetchData = async () => {
     try {
-        // const response = await fetch('http://localhost:8000/jobs');
         const response = await fetch('http://localhost:8000/jobs', {
         method: 'POST',
         headers: {
--- applitrackerproject/src/Login.js
+++ applitrackerproject/src/Login.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import { Container, Row, Col, Form, FormGroup, Label, Input, Button } from 'reactstrap';
-import { Link } from "react-router-dom";
-import { useNavigate  } from 'react-router-dom';
-// import './css/login.css';
+import { Link, useNavigate } from "react-router-dom";
 
 const Login = () => {
   const navigate  = useNavigate (); 
@@ -67,56 +65,3 @@ const Login = () => {
 };
 
 export default Login;
-
-
-
-
-
-
-
-
-
-
-// function Login() {
-
-//   const navigate = useNavigate();
-
-//   // Dummy login function for demonstration
-//   const handleLogin = async (event) => {
-//     event.preventDefault(); // Prevent the default form submission
-    
-//     // If login is successful, navigate to Dashboard
-//     navigate('/home_dashboard');
-//   };
-
-//   return (
-//     <div className="App">      
-//       <main>
-//         <div className="login-form">
-//           <h2>Login</h2>
-//           <form onSubmit={handleLogin}>
-//               <div>
-//                 <div className="form-group">
-//                   <input type="id" name="email_id" id="email_id" placeholder="Enter your id" required/>
-//                   <br />
-//                 </div>
-//                 <div className="form-group">
-//                   <input type="password" name="password" id="password"  placeholder="Enter your password" required/>
-//                   <br />
-//                 </div>
-//                 <Button color="primary" type="submit">Login</Button>
-//                 <div>
-//                 <Link to="/signup">
-//                   <Button color="secondary">Sign Up</Button>
-//                 </Link>
-//                 </div>
-               
-//               </div>
-//           </form>
-//         </div>
-//       </main>     
-//     </div>
-//   );
-// };
-
-// export default Login;
