f7f3ba0eefa20fcdf0af875902f8fc915273e4e8
Parva Shah <parvashah310501@gmail.com>
2024-04-22 20:40:16 -0400

    Login testing

--- backend/controllers/userLoginController.js
+++ backend/controllers/userLoginController.js
@@ -1,10 +1,8 @@
-// userLoginController.js
+const User = require('../models/user');
 
-const User = require('../models/user'); // Import your user model
-
-// Handle user login
 const userLogin = async (req, res) => {
   const { email, password } = req.body;
+
 
   try {
     const user = await User.findOne({ email });
@@ -12,15 +10,17 @@ const userLogin = async (req, res) => {
     if (!user) {
       return res.status(404).json({ message: 'User not found' });
     }
+
     const isValidPassword = await user.isValidPassword(password);
+
     if (!isValidPassword) {
       return res.status(401).json({ message: 'Invalid email or password' });
     }
 
-    res.status(200).json({ message: 'Login successful', user });
+    return res.status(200).json({ message: 'Login successful', user: { email } });
   } catch (error) {
     console.error('Login error:', error);
-    res.status(500).json({ message: 'Internal server error' });
+    return res.status(500).json({ message: 'Internal server error' });
   }
 };
 
